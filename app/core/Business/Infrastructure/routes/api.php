ae1c57de3d26ed715cb7215499cccfdb53b0241a
stevelee <hoang.le.tn91@gmail.com>
2026-02-10 05:59:03 +0700

    update: change middleware alias name

--- app/core/Business/Infrastructure/routes/api.php
+++ app/core/Business/Infrastructure/routes/api.php
@@ -3,13 +3,13 @@
 use Core\Business\Http\Controllers\BusinessController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/api')->middleware(['isLogged', 'app.language'])->group(function () {
+Route::prefix('/api')->middleware(['app.isLogged', 'app.language'])->group(function () {
     Route::get('/business', [BusinessController::class, 'index']);
     Route::get('/business/{id}', [BusinessController::class, 'show']);
     Route::middleware(['business'])->group(function () {
         Route::put('/business/{id}', [BusinessController::class, 'update']);
     });
-    Route::middleware(['IsAdmin'])->group(function () {
+    Route::middleware(['app.isAdmin'])->group(function () {
         Route::post('/business', [BusinessController::class, 'store']);
     });
 });
