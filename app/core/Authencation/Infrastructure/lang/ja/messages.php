9227b15577c591d7243303cee34e14e46907dc1f
stevelee <hoang.le.tn91@gmail.com>
2026-02-10 23:22:46 +0700

    fix: Update language files for vi and ja of authencation

--- app/core/Authencation/Infrastructure/lang/ja/messages.php
+++ app/core/Authencation/Infrastructure/lang/ja/messages.php
@@ -1,7 +1,17 @@
 <?php
 
 return [
-    'not_verify' => 'アカウントがまだ認証されていません。メールの受信箱をご確認ください。',
-    'not_found'  => 'アカウントが見つかりません。',
-    'email_used' => 'このメールアドレスはすでに使用されています。',
+    'not_verify'               => 'アカウントがまだ認証されていません。メールの受信箱をご確認ください。',
+    'not_found'                => 'アカウントが見つかりません。',
+    'email_used'               => 'このメールアドレスはすでに使用されています。',
+    'not_logged'               => 'ログインしていません。',
+    'password_is_not_correctly'=> 'パスワードが正しくありません。',
+    'password_is_required'     => 'パスワードは必須です。',
+    'new_password_is_required' => '新しいパスワードは必須です。',
+    'changed_password'         => 'パスワードが :password に変更されました。',
+    'security_account'         => 'アカウントのセキュリティ',
+    'message_verify_account'   => 'このメールはアカウントを認証するためのものです。',
+    'subject_verify_account'   => 'アカウント認証',
+    'message_reset_password'   => 'このメールはパスワードをリセットするためのものです。',
+    'subject_reset_password'   => 'パスワードのリセット',
 ];
--- app/core/Authencation/Infrastructure/lang/vi/messages.php
+++ app/core/Authencation/Infrastructure/lang/vi/messages.php
@@ -1,7 +1,17 @@
 <?php
 
 return [
-    'not_verify' => 'Tài khoản của bạn chưa được xác thực, vui lòng kiểm tra hộp thư email',
-    'not_found'  => 'Không tìm thấy tài khoản',
-    'email_used' => 'Email này đã được sử dụng',
+    'not_verify'               => 'Tài khoản của bạn chưa được xác thực, vui lòng kiểm tra hộp thư email',
+    'not_found'                => 'Không tìm thấy tài khoản',
+    'email_used'               => 'Email này đã được sử dụng',
+    'not_logged'               => 'Bạn chưa đăng nhập',
+    'password_is_not_correctly'=> 'Mật khẩu không chính xác',
+    'password_is_required'     => 'Mật khẩu là bắt buộc',
+    'new_password_is_required' => 'Mật khẩu mới là bắt buộc',
+    'changed_password'         => 'Mật khẩu của bạn đã được thay đổi thành :password',
+    'security_account'         => 'Bảo mật tài khoản',
+    'message_verify_account'   => 'Đây là email để xác thực tài khoản của bạn',
+    'subject_verify_account'   => 'Xác thực tài khoản',
+    'message_reset_password'   => 'Đây là email để đặt lại mật khẩu',
+    'subject_reset_password'   => 'Đặt lại mật khẩu',
 ];
