5fc2614690f9e10b236db6e5c44b170d8ff76d7b
stevelee <hoang.le.tn91@gmail.com>
2026-02-13 04:23:18 +0700

    refactor: change logic implement console run by schedule from console default to schedule service provider. fix: error stuck overlapping for queue

--- app/routes/console.php
+++ app/routes/console.php
@@ -3,13 +3,8 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-if(env('APP_ENV') === 'production') {
-    Schedule::command('app:overview')->daily();
-} else {
-    Schedule::command('app:overview')->everyFiveMinutes();
-}
 Schedule::command(
-    'queue:work --queue=high,default,low --sleep=3 --tries=3 --timeout=30 --max-jobs=1'
+    'queue:work --queue=high,default,low --tries=3 --timeout=75 --max-jobs=1 --max-time=4'
 )
 ->everyFiveSeconds()
-->withoutOverlapping();
+->withoutOverlapping(2);
