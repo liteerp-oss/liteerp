24cb00a3d32c479b66bcf4e5c9407554a848096e
stevelee <hoang.le.tn91@gmail.com>
2026-02-21 13:13:08 +0700

    refactor: add type for StatusBadge component

--- app/resources/js/react/components/StatusBadge.jsx
+++ app/resources/js/react/components/StatusBadge.jsx
@@ -2,8 +2,10 @@ import { useI18n } from "@/i18n/useI18n"
 import React from "react"
 
 export default function StatusBadge({
-    status = null 
+    status = null,
+    type = null 
 }){
     const {t} = useI18n();
-    return <span className={"badge text-uppercase rounded-pill px-3 py-2 badge-" + status}>{t(status)}</span>
+    return <span className={"badge text-uppercase rounded-pill px-3 py-2 badge-" 
+            + ( type ?? status)}>{t(status)}</span>
 }
