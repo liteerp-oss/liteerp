d2f6d70d08a83cc7a33721cfbca459ba86f9e34c
stevelee <hoang.le.tn91@gmail.com>
2026-02-21 13:16:35 +0700

    refactor: add check role option for TabsCommon component

--- app/resources/js/react/components/TabsCustom.jsx
+++ app/resources/js/react/components/TabsCustom.jsx
@@ -3,12 +3,13 @@ import { useSelector } from 'react-redux';
 export default function TabsCommon({
     navs = [],
     contents = [],
+    checkRole = true 
 }) {
     const roles = useSelector((state) => state.businessRole.role);
     return <div>
         <ul className="nav nav-tabs" id="myTab" role="tablist">
             {navs.map((item,index) => {
-                return roles?.includes('erp.' + item.key + '.index') ? <li key={index} className="nav-item" role="presentation">
+                return roles?.includes(item.key) || !checkRole ? <li key={index} className="nav-item" role="presentation">
                     <button className={"nav-link " + (index === 0 ? 'active' : '')} id={item.key + "-tab"} data-bs-toggle="tab" 
                     data-bs-target={"#" + item.key} type="button" role="tab" 
                     aria-controls={item.key} 
@@ -18,7 +19,7 @@ export default function TabsCommon({
         </ul>
         <div className="tab-content" id="myTabContent">
             {navs.map((item,index) => {
-                return roles?.includes('erp.' + item.key + '.index') ? <div key={index} 
+                return roles?.includes(item.key) || !checkRole  ? <div key={index} 
                 className={"mt-2 tab-pane fade " + (index === 0 ? ' show active' : '')} id={item.key} role="tabpanel" 
                 aria-labelledby={item.key + "-tab"}>
                     {contents[index]}
