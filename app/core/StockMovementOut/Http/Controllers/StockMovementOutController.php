50079e30dfaa1ef6d798f93b524ae286704c4f59
stevelee <hoang.le.tn91@gmail.com>
2026-02-26 17:48:54 +0700

    refactor: stockmovementout -> remove method create and update manual

--- app/core/StockMovementOut/Http/Controllers/StockMovementOutController.php
+++ app/core/StockMovementOut/Http/Controllers/StockMovementOutController.php
@@ -2,31 +2,11 @@
 
 namespace Core\StockMovementOut\Http\Controllers;
 
-use Core\StockMovementOut\Application\UseCases\CreateStockMovementOut;
-use Core\StockMovementOut\Application\DTOs\CreateStockMovementOutRequest;
 use Core\StockMovementOut\Application\Queries\IndexQuery;
-use Core\StockMovementOut\Application\UseCases\UpdateStockMovementOut;
-use Core\StockMovementOut\Http\Requests\CreateStockMovementOutRequest as FormRequest;
 use Core\StockMovementOut\Http\Requests\IndexStockMovementOutRequest;
-use Core\StockMovementOut\Http\Requests\UpdateStockMovementOutRequest;
 
 class StockMovementOutController
 {
-    public function store(FormRequest $request, 
-        CreateStockMovementOut $useCase)
-    {
-        $dto = CreateStockMovementOutRequest::fromArray($request->all());
-        $entity = $useCase->handle($dto);
-        return response()->json(['message' => $entity]);
-    }
-    public function update(UpdateStockMovementOutRequest $request,
-        UpdateStockMovementOut $useCase,string $id)
-    {
-        $request->merge(['id' => $id]);
-        $dto = UpdateStockMovementOutRequest::fromArray($request->all());
-        $entity = $useCase->handle($dto);
-        return response()->json(['message' => $entity]);
-    }
     public function index(IndexStockMovementOutRequest $request,
         IndexQuery $useCase) {
             $entity = $useCase->handle($request->all());
