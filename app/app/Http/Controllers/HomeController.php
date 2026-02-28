8e54d5a8fe0778f7cf5d410cbc20427c615a6a17
stevelee <hoang.le.tn91@gmail.com>
2026-02-28 14:26:47 +0700

    refactor: add hook for method index home controller

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -1,8 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Supports\Hooks\HookAction;
+use App\Supports\Hooks\HookContext;
+use App\Supports\Hooks\HookDispatcher;
+use App\Supports\Hooks\HookPhase;
+use App\Supports\Hooks\HookTiming;
+
 class HomeController extends Controller {
-    public function index() {
-       return view("welcome");
+    public function index(HookDispatcher $hooks) {
+        $view = "welcome";
+        $data = $hooks->dispatch(new HookContext(
+            action: HookAction::INDEX,
+            phase: HookPhase::UI,
+            timing: HookTiming::ON,
+            module: 'Home',
+            payload: [
+                'view' => $view 
+            ]
+        ));
+        return view($data['view'],$data);
     }
 }
