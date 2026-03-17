69d41fb4b07f0208e663449a85cef5ff1af2bfb7
stevelee <hoang.le.tn91@gmail.com>
2026-03-18 04:19:39 +0700

    refactor: update display content table product

--- app/resources/js/react/components/Order/EditOrder/Products/ListProduct.jsx
+++ app/resources/js/react/components/Order/EditOrder/Products/ListProduct.jsx
@@ -5,23 +5,41 @@ import { useForm } from "../../../../libraries/handleInput";
 import InventoryService from '../../../../services/InventoryService'
 import { useI18n } from "@/i18n/useI18n";
 import CommonDataTableV2 from "@/react/components/CommonDataTableV2";
+import StockMovementInService from "@/react/services/StockMovementInService";
+import ContentOnTable from "@/react/components/ContentOnTable";
 export default function ListProduct({
     add = (product) => { },
-    detail = null 
+    detail = null
 }) {
-    const {t} = useI18n();
+    const { t } = useI18n();
     const columns = [
         { label: "ID", key: "id" },
-        { label: t("Name"), key: "name" },
+        {
+            label: "PU", key: "purchase_id", render: (value) => {
+                return "PU" + value
+            }
+        },
+        {
+            label: t("Name"), key: "name", render: (value) => {
+                return <ContentOnTable value={value} />
+            }
+        },
         { label: t("Quantity"), key: "quantity" },
-        { label: t("Category"), key: "category" },
+        { label: t("Category"), key: "category",render: (value) => {
+                    return <ContentOnTable value={value}/>
+                }  },
         {
             label: t("Price"), key: "price", render: (value) => {
-                return <Currencies amount={value}/>;
+                return <Currencies amount={value} />;
             }
         },
         {
             label: t("Warehouse"), key: "warehouse", render: (value) => {
+                return <ContentOnTable value={value} />
+            }
+        },
+        {
+            label: t("Purchase date"), key: "purchase_date", render: (value) => {
                 return value;
             }
         }
@@ -30,8 +48,7 @@ export default function ListProduct({
     const search = useForm();
     const getInventories = useCallback((page = 0) => {
         table.setLoading(true);
-        InventoryService.list({
-            ...search.formData,
+        StockMovementInService.list({
             ...detail,
             page: page,
         })
@@ -44,32 +61,32 @@ export default function ListProduct({
             .catch((error) => {
 
             })
-    }, [search.formData,detail]);
+    }, [search.formData, detail]);
     useEffect(() => {
         getInventories();
-    },[detail?.customer_group_id])
+    }, [detail?.customer_group_id])
     return <div className="mt-3">
         <h4 className="h5">{t("Inventory")}</h4>
         <CommonDataTableV2
             config={{
-                        default: [{
-                            key: "order_by",
-                            placeholder: t("Order by"),
-                            options: [
-                                { value: 'ASC', label: t('Oldest') },
-                                { value: 'DESC', label: t('Newest') },
-                            ],
-                            type: "select",
-                            label: t("Order by"),
-                            col: "col-6"
-                        }, {
-                            key: "keywords",
-                            placeholder: t("Keywords"),
-                            type: "text",
-                            label: t("Search"),
-                            col: "col-6"
-                        }]
-                    }}
+                default: [{
+                    key: "order_by",
+                    placeholder: t("Order by"),
+                    options: [
+                        { value: 'ASC', label: t('Oldest') },
+                        { value: 'DESC', label: t('Newest') },
+                    ],
+                    type: "select",
+                    label: t("Order by"),
+                    col: "col-6"
+                }, {
+                    key: "keywords",
+                    placeholder: t("Keywords"),
+                    type: "text",
+                    label: t("Search"),
+                    col: "col-6"
+                }]
+            }}
             loading={table.loading}
             columns={columns}
             data={table?.data}
