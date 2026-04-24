84a7d642079f40d4c26be6e84350025fe0914636
stevelee <hoang.le.tn91@gmail.com>
2026-04-24 19:02:54 +0700

    refactor: update listener for write logs

--- app/core/ActivityLog/Infrastructure/Listeners/ActivityLogWrite.php
+++ app/core/ActivityLog/Infrastructure/Listeners/ActivityLogWrite.php
@@ -13,31 +13,22 @@ class ActivityLogWrite
     public function __construct(private CreateActivityLog $createLog) {}
     public function handle()
     {
-        Event::listen('erp.*.*', function (string $eventName, array $data) {
-            $name = explode('.', $eventName);
-            if (count($name) === 3) {
-                $module = $name[0];
-                $event = $name[1];
-                $action = $name[2];
-                if($action === 'index' || $action === 'show') {
-                    return;
-                }
-                if (
-                    empty($data['user_id'])
-                    || empty($data['business_id'])
-                    || empty($data['id'])
-                ) {
-                    return;
-                }
-                $this->createLog->handle(CreateActivityLogRequest::fromArray([
-                    'user_id' => $data['user_id'],
-                    'action' => $action,
-                    'description' => $data,
-                    'entity_type' => $event,
-                    'entity_id' => $data['id'],
-                    'business_id' => $data['business_id']
-                ]));
-            } 
+        Event::listen('erp.activitylog.*', function (string $eventName, array $data) {
+            if (
+                empty($data['user_id'])
+                || empty($data['business_id'])
+                || empty($data['id'])
+            ) {
+                return;
+            }
+            $this->createLog->handle(CreateActivityLogRequest::fromArray([
+                'user_id' => $data['user_id'],
+                'action' => str_replace('erp.activitylog.', '', $eventName),
+                'description' => $data['data'] ?? [],
+                'entity_type' => $data['type'],
+                'entity_id' => $data['id'],
+                'business_id' => $data['business_id']
+            ]));
         });
     }
 }
