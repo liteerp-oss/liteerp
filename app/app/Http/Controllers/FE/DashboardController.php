f8cddbc06b359240b8eb1f7bbf0e5b064d1c3b82
stevelee <hoang.le.tn91@gmail.com>
2026-04-28 00:50:57 +0700

    refactor: add child view hook dashboard controller

--- app/app/Http/Controllers/FE/DashboardController.php
+++ app/app/Http/Controllers/FE/DashboardController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\FE;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Supports\Hooks\HookAction;
+use App\Supports\Hooks\HookContext;
+use App\Supports\Hooks\HookDispatcher;
+use App\Supports\Hooks\HookPhase;
+use App\Supports\Hooks\HookTiming;
 
 class DashboardController extends Controller
 {
     //
-    public function index(){
-        return view('admin.dashboard.index');
+    public function index(HookDispatcher $hooks){
+        $data = $hooks->dispatch(new HookContext(
+            action: HookAction::INDEX,
+            phase: HookPhase::UI,
+            timing: HookTiming::ON,
+            module: 'Dashboard',
+            payload: [
+                'childViews' => []
+            ]
+        ));
+        return view('admin.dashboard.index',$data);
     }
 }
