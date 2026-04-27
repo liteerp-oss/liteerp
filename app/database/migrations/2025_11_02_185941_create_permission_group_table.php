21b9cbb3d80cdafd0a9b8e34fae9707a78bf0e8f
stevelee <hoang.le.tn91@gmail.com>
2026-04-28 00:54:30 +0700

    fix: fix unique name for permission group

--- app/database/migrations/2025_11_02_185941_create_permission_group_table.php
+++ app/database/migrations/2025_11_02_185941_create_permission_group_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('permission_groups', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100)->unique();
+            $table->string('name', 100);
             $table->string('type', 100)->nullable();
             $table->timestamps();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
